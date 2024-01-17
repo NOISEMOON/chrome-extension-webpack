f60f7fb0ec43cf39feb747992a01f7934417db0c
boris.li <boris.li@zoom.us>
2024-01-17 10:00:22 +0800

    append div

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,7 +2,12 @@ import { ActionTypeEnum, MessageTypeEnum, TargetLanguageEnum } from "./constant"
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.type === MessageTypeEnum.SELECT_TEXT) {
-        alert(message.data.aiResult);
+        const rangeCount = window.getSelection().rangeCount;
+        const range = window.getSelection().getRangeAt(rangeCount - 1);
+        const div = document.createElement("div");
+        div.innerHTML = message.data.aiResult;
+        div.style.color = "red";
+        range.insertNode(div);
     }
     return true;
 });
